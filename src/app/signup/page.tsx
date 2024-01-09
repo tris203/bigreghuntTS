a3a45909d494a274c56b7f2344df308441b35f12
Tristan Knight <admin@snappeh.com>
2024-01-09 22:03:53 +0000

    feat: prettier signup form (#7)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -23,15 +23,38 @@ async function processSignup(formdata: FormData) {
 
 function Signup() {
   return (
-    <div>
-      <h1>Signup</h1>
-      <div>
-        <form action={processSignup}>
-          <input type='text' placeholder='Nickname' name='nickname' />
-          <input type='text' placeholder='Email' name='email' />
-          <input type='password' placeholder='Password' name='password' />
-          <input type='submit' value='Signup' />
-        </form>
+    <div className='flex w-full flex-col items-center justify-center py-4'>
+      <div className='mx-audo container flex max-w-sm flex-1 flex-col items-center justify-center px-2 py-2'>
+        <div className='w-full rounded bg-white px-6 py-8 text-black shadow-md'>
+          <h1 className='mb-8 text-center text-3xl'>Signup</h1>
+          <div>
+            <form action={processSignup}>
+              <input
+                type='text'
+                className='border-grey light mb-4 block w-full rounded border p-3'
+                placeholder='Nickname'
+                name='nickname'
+              />
+              <input
+                type='text'
+                className='border-grey light mb-4 block w-full rounded border p-3'
+                placeholder='Email'
+                name='email'
+              />
+              <input
+                type='password'
+                className='border-grey light mb-4 block w-full rounded border p-3'
+                placeholder='Password'
+                name='password'
+              />
+              <input
+                type='submit'
+                className='my-1 w-full rounded bg-green-600 py-3 text-center text-white hover:bg-green-500 focus:outline-none'
+                value='Signup'
+              />
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
